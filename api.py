e954952e0d78b330e3627568ead2205fd1700819
william-song-shy <2560814258@qq.com>
2022-02-17 09:02:12 +0800

    Remake Searching API

--- api.py
+++ api.py
@@ -9,18 +9,21 @@ def search_poem (string):
     # for i in data.children:
     #     l.append(i.string)
     # return l[0],l[1][1:-1]
-    url='https://so.gushiwen.cn/search.aspx?value={}'.format(string)
+    url='https://so.gushiwen.cn/search.aspx?value={}&type=mingju'.format(string)
     r=requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     data = soup.find('div',class_='sons')
+    if not data:
+        return None
     # print (data)
-    line = data.find('span',style='color:#B00815')
-    if (not line):
+    line = data.find('span')
+    # print (data)
+    if (not line or line.text != string):
         return None
     # line=line.parent.text
-    title=data.find('p').text.replace('\n','')
-    author=data.find('p',class_='source').text.replace('\n','')
-    return title,author#,line
+    # print(data.find_all('a')[1].text)
+    author,title = data.find_all('a')[1].text.split('《')
+    return title[:-1],author#,line
 
 if __name__ == '__main__':
     print(search_poem(input()))
