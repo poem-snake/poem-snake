9c68f828ca0fd9018fe3eae30f1069b42478a679
william-song-shy <2560814258@qq.com>
2022-02-18 15:08:03 +0800

    fix a huge bug

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ socket_io = SocketIO(app,logger=True, engineio_logger=True)
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
-    password = db.Column(db.String(20))
+    password = db.Column(db.String(100))
     admin = db.Column(db.Boolean, default=False)
     email = db.Column(db.String(50), unique=True)
 
